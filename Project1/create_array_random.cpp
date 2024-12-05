7bcfe52a2ddcda78468c5ff9a7f6a8c433531c98
pato4ok2020 <stepananikeev2019@gmail.com>
2024-12-05 19:15:56 +0300

    Улучшена функция рандомного создавания массива

--- Project1/create_array_random.cpp
+++ Project1/create_array_random.cpp
@@ -8,9 +8,12 @@
 using namespace std;
 void create_array_random(double* nums, int N)
 {
+	srand(time(0));
+	int start = -10;
+	int end = 10;
 	for (int i = 0; i < N; i++)
 	{
-		nums[i] = rand() % 10;
+		nums[i] = rand() % (end - start + 1) + start;
 		cout << nums[i] << " ";
 	}
 }
